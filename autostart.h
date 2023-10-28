4f906fa62ac2cb4032acc20cc6702038406d42c6
Lumetas <98038223+Lumetas@users.noreply.github.com>
2023-10-28 11:21:56 +0500

    Add files via upload

--- autostart.h
+++ autostart.h
@@ -1,8 +1,8 @@
 void
 runAutostart(void) {
 	system("feh --bg-scale wallpaper/bg6.jpg &");
-	system("compton -f --vsync &");
+	system("compton -f --vsync opengl-oml &");
 	system("~/dwm/status &");
-	system("xss-lock lock &");
+	system("xss-lock ~/dwm/lock &");
 	system("sleep 2 && setxkbmap -option 'ctrl:nocaps' ; setxkbmap 'us,ru' ',winkeys' 'grp:alt_shift_toggle' &");
 }
