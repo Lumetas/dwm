4d88abda08ff25952e7760c66f1697006d77db02
lum <lum@lumetas.lumetas.ru>
2026-03-12 13:03:51 +0500

    firefox rule

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const char col_bar_urgent[]  = "#5A5775";   /* Срочный тег */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
+#define TAG(n) (1 << ((n) - 1))
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -71,7 +71,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL, "Navigator", NULL, TAG(1), 0, -1 },
 };
 
 /* layout(s) */
